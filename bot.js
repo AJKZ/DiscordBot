a1b85f6cdc4bdcfbe3cb21fb56b8b374aaed1b61
Alan Zhong <0977837@hr.nl>
2019-08-25 10:25:18 +0200

    Add working game lobby posting and message pruning

--- bot.js
+++ bot.js
@@ -1,37 +1,65 @@
 const fs = require('fs');
 const Discord = require('discord.js');
-const { config } = require('dotenv');
 
-// Set environment file
-config({path: __dirname + '/.env'});
+// environment variables
+require('dotenv').config();
+const PREFIX = process.env.PREFIX;
 
 const bot = new Discord.Client();
-
-// Get commands from directory
 bot.commands = new Discord.Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-
 for(const file of commandFiles) {
     const cmd = require(`./commands/${file}`);
-
     // set a new item in the Collection
 	// with the key as the command name and the value as the exported module
     bot.commands.set(cmd.name, cmd);
 }
-
-const PREFIX = process.env.PREFIX;
 
 bot.once('ready', () => {
     console.log(bot.user.username + ' is online. \n');
 });
 
 bot.on('message', message => {
-    if(message.author.bot || !message.guild || !message.content.startsWith(PREFIX)) {
-        return;
+    // Check whether author is the bot OR 
+    // the message does not start with the prefix
+    if(message.author.bot ||
+        !message.content.startsWith(PREFIX)) { return; }
+
+    // get the commands and arguments after prefix
+    const args = message.content.slice(PREFIX.length).split(/ +/);
+    const cmdName = args.shift().toLocaleLowerCase();
+
+    // find command
+    const cmd = bot.commands.get(cmdName)
+        || bot.commands.find(cmd => cmd.aliases && cmd.aliases.includes(cmdName));
+
+    if(!cmd) { return; }
+    
+    // check for DM command
+    if(cmd.guildOnly && message.channel.type !== 'text') {
+        return message.reply(`Command can't be executed in DM.`);
     }
 
-    const args = message.content.slice(PREFIX.length).split(/ +/g);
-    const cmd = args.shift().toLocaleLowerCase();
+    // if a command that requires arguments was used
+    // without arguments provided 
+    if(cmd.args && !args.length) {
+        let reply = `No arguments provided.`;
+
+        if(cmd.usage) {
+            reply += `\nUsage: \`${PREFIX}${cmd.name} ${cmd.usage}\``;
+        }
+
+        return message.channel.send(reply);
+    }
+
+    // execute command
+    try {
+        cmd.execute(bot, message, args);
+    }
+    catch (error) {
+        console.log(error);
+        message.reply('Error trying to execute that command.');
+    }
 });
 
 bot.login(process.env.TOKEN);
